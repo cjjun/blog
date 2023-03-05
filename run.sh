f2bdb27e6cd10439bd14d9ed6fc9a6156227f8d1
cjjun <cjjst123@gmail.com>
2023-03-05 17:45:49 +0800

    resources

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # npm install --save hexo-katex-macros
 # npm install hexo-deployer-git --save 
 
-/usr/local/bin/hexo clean
-/usr/local/bin/hexo g
-/usr/local/bin/hexo d
-/usr/local/bin/hexo clean
+hexo clean
+hexo g
+hexo d
+hexo clean
